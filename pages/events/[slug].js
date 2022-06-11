11bcb15e4e39a9e2ec751791b32053af98ccb3a2
Wiji <peawseaw@gmail.com>
2022-06-11 14:58:43 -0700

    Connecting to the Strapi API

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -25,21 +25,21 @@ export default function EventPage ({evt}) {
                     </a>
                 </div>
                 <span>
-                    {evt.date} at {evt.time}
+                {new Date(evt.attributes.date).toLocaleDateString('en-US')} at {evt.attributes.time}
                 </span>
-
-                <h1>{evt.name}</h1>
-                {evt.image && (
+                {evt.attributes.image[0]}
+                <h1>{evt.attributes.name}</h1>
+                {evt.attributes.image && (
                     <div className={styles.image}>
-                        <Image src={evt.image} width={960} height={600} />
+                        <Image src={evt.attributes.image.data.attributes.formats.thumbnail.url} width={960} height={600} />
                     </div>
                 )}
                 <h3>Performer:</h3>
-                <p>{evt.performer}</p>
+                <p>{evt.attributes.performer}</p>
                 <h3>Description:</h3>
-                <p>{evt.description}</p>
-                <h3>Venue: {evt.venue}</h3>
-                <p>{evt.address}</p>
+                <p>{evt.attributes.description}</p>
+                <h3>Venue: {evt.attributes.venue}</h3>
+                <p>{evt.attributes.address}</p>
 
                 <Link href='/events'>
                     <a className={styles.back}>{'<'} Go Back</a>
@@ -49,42 +49,42 @@ export default function EventPage ({evt}) {
     )
 }
 
-// export async function getStaticPaths() {
-//   const res = await fetch(`${API_URL}/api/events`)
-//   const events = await res.json()
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}api/events`)
+  const events = await res.json()
 
-//   const paths = events.map((evt) => ({
-//     params: { slug: evt.slug },
-//   }))
+  const paths = events.data.map((evt) => ({
+    params: { slug: evt.attributes.slug },
+  }))
 
-//   return {
-//     paths,
-//     fallback: true,
-//   }
-// }
-
-// export async function getStaticProps({ params: { slug } }) {
-//   const res = await fetch(`${API_URL}/events/api/events/${slug}`)
-//   const events = await res.json()
-
-//   return {
-//     props: {
-//       evt: events[0],
-//     },
-//     revalidate: 1,
-//   }
-// }
-
-export async function getServerSideProps({query:{slug}}){
-    const res = await fetch(`${API_URL}/api/events/${slug}`)
-    const events = await res.json()
-    
-    return{
-        props:{
-            evt:events[0]
-        },
-    }
+  return {
+    paths,
+    fallback: true,
+  }
 }
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}api/events?populate=*?slug=${slug}`)
+  const events = await res.json()
+
+  return {
+    props: {
+      evt: events.data[0],
+    },
+    revalidate: 1,
+  }
+}
+
+// export async function getServerSideProps({query:{slug}}){
+//     const res = await fetch(`${API_URL}/api/events?populate=*?slug=${slug}`)
+//     const events = await res.json()
+    
+//     return{
+//         props:{
+//             evt:events[0]
+//         },
+//     }
+// }
 
 
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -8,7 +8,7 @@ export default function EventsPage({events}) {
       <h1>Events</h1>
       {events.length === 0 && <h3>No events to show</h3>}
 
-      {events.map((evt) => (
+      {events.data.map((evt) => (
         <EventItem key={evt.id} evt={evt}/>
       ))}
     </Layout>
@@ -16,7 +16,7 @@ export default function EventsPage({events}) {
 }
 
 export async function getStaticProps(){
-  const res = await fetch(`${API_URL}/api/events`)
+  const res = await fetch(`${API_URL}api/events?populate=*?_sort=date:ASC`)
   const events = await res.json()
 
   return {
